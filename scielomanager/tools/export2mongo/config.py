eca2af8e687ce841053136c97dbc2d0c336d8779
Juan Funez <juan.funez@gmail.com>
2015-11-17 16:46:42 -0200

    importação do Journal para o mongodb

---
+++ scielomanager/tools/export2mongo/config.py
@@ -0,0 +1,6 @@
+# Constants
+
+MONGODB_SETTINGS = {
+    'name': 'manager2mongo',
+    'host': '127.0.0.1',
+}
---
+++ scielomanager/tools/export2mongo/main.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import os
+import json
+from datetime import datetime
+from pprint import pprint
+from mongoengine import *
+import config
+from documents_definitions import *
+
+# DJANGO IMPORTS
+from django.core.management import setup_environ
+from django.core.exceptions import ObjectDoesNotExist
+
+try:
+    from scielomanager import settings
+except ImportError:
+    BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    BASE_PATH_APP = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'scielomanager'))
+    from sys import path
+    path.append(BASE_PATH)
+    path.append(BASE_PATH_APP)
+
+    import settings
+
+setup_environ(settings)
+from journalmanager.models import Journal, Issue, Article
+
+connect(config.MONGODB_SETTINGS['name'])
+
+
+for journal in Journal.objects.all():
+    djournal = DJournal(**journal_to_djournal(journal))
+    djournal.save()
+
+    print 'Save journal: ', journal.id, ' with id: ', djournal.id
